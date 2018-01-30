6c9bf0c93bbe862140f61ebf66f1f8d683029be9
Søren Pedersen <sh.pedersen@gmail.com>
2018-01-30 13:58:29 +0100

    Create the example implementation of how to generate a business generated link

--- ExampleImplementation/Program.cs
+++ ExampleImplementation/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace ExampleImplementation
+﻿namespace ExampleImplementation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using AuthenticatedEncryption;
+    using Newtonsoft.Json;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var domain = "INSERT YOUR DOMAIN HERE";
+            var cryptKeyString = "INSERT YOUR CRYPT KEY HERE";
+            var authKeyString = "INSERT YOUR AUTH KEY HERE";
+            var order = new Order
+                            {
+                                Email = "INSERT CONSUMER EMAIL ADDRESS",
+                                Name = "INSERT CONSUMER NAME",
+                                Ref = "INSERT ORDER REFERENCE NUMBER",
+                                Skus = new List<string> { "INSERT SKUS HERE" },
+                                Tags = new List<string> { "INSERT TAGS HERE" }
+                            };
+            var jsonEncodedOrder = JsonConvert.SerializeObject(order);
+            var cryptKey = Convert.FromBase64String(cryptKeyString);
+            var authKey = Convert.FromBase64String(authKeyString);
+
+            var cipherText = AuthenticatedEncryption.Encrypt(jsonEncodedOrder, cryptKey, authKey);
+            var urlEncodedCipherText = WebUtility.UrlEncode(cipherText);
+
+            Console.WriteLine($"https://www.trustpilot.com/evaluate-bgl/{domain}?p={urlEncodedCipherText}");
+        }
+
+        public class Order
+        {
+            public string Email { get; set; }
+
+            public string Name { get; set; }
+
+            public string Ref { get; set; }
+
+            public List<string> Skus { get; set; }
+
+            public List<string> Tags { get; set; }
         }
     }
 }
